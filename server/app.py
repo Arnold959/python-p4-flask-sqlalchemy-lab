ab730da1bdecd4639a900a429d283044ce502ca7
arnold <musaliokello@gmail.com>
2023-06-30 23:22:40 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, abort, jsonify
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
@@ -17,18 +17,85 @@ db.init_app(app)
 def home():
     return '<h1>Zoo app</h1>'
 
-@app.route('/animal/<int:id>')
-def animal_by_id(id):
-    return ''
+@app.route('/animal')
+def get_animals():
+    animals_list = []
+    for animal in Animal.query.all():
+        animal_dict ={
+         "id": animal.id,
+         "name": animal.name,
+         "species": animal.species,
+           
+        }
+        animals_list.append(animal_dict)
 
-@app.route('/zookeeper/<int:id>')
-def zookeeper_by_id(id):
-    return ''
+    response = make_response(
+        jsonify(animal_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-@app.route('/enclosure/<int:id>')
-def enclosure_by_id(id):
-    return ''
+@app.route('/animal/<id>')
+def get_animal(id):
+    animal = Animal.query.get(id)
+    if not animal:
+        abort (404)
+
+    animal_dict = {
+         "id": animal.id,
+         "name": animal.name,
+         "species": animal.species,
+    }
+
+    response = make_response(
+        jsonify(animal_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+@app.route('/zookeeper/<id>')
+def zookeeper_route(id):
+    zookeeper = Zookeeper.query.get(id)
+    if not zookeeper:
+      abort (404)
+    
+    zookeeper_dict = {
+        "id": zookeeper.id,
+        "name": zookeeper.name,
+        "birthday": zookeeper.birthday,
+    }
+
+    response = make_response(
+        jsonify (zookeeper_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+@app.route('/enclosure/<id>')
+def enclosure_route(id):
+    enclosure = Enclosure.query.get(id)
+    if not enclosure:
+        abort (404) 
+
+    enclosure_dict = {
+        "id": enclosure.id,
+        "environment": enclosure.environment,
+        "open_to_visitors": enclosure.open_to_visitors,
+    }
+
+    response = make_response(
+        jsonify(enclosure_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+  
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=6000, debug=True)
